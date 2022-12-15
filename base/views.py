04fcacfba8f5297559872ab87d87e12674f33edc
unknown <omaramin622@gmail.com>
2022-12-15 11:21:35 +0200

    add chat room feature

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.contrib import messages
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .form import RoomForm
 
 
@@ -80,7 +80,21 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    # get all children models(message) of model # see the relation
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body'),
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages,
+               'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -129,4 +143,20 @@ def deleteRoom(request, pk):
         return redirect('home')
 
     context = {'obj': room}
+    return render(request, 'base/delete.html', context)
+
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    # host is the only one can delete the room
+    if request.user != message.user:
+        return HttpResponse('You have not a permission')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room', pk=message.room.id)
+
+    context = {'obj': message}
     return render(request, 'base/delete.html', context)
